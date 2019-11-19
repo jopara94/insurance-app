d3bbc0836cf2ab433722967eb158a69ae6630439
James Opara <jamesopara@Jamess-MacBook-Pro.local>
2019-11-18 22:41:53 -0600

    commit

---
+++ backend/insurance/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+class Member(models.Model):
+    first_name = models.CharField(max_length=30, default='')
+    last_name = models.CharField(max_length=30, default='')
+    phone_number = models.CharField(max_length=15, default='', null=True)
+
+    def __str__(self):
+        return f'Hi {self.first_name}, Welcome to your profile'
+
+class Dependent(models.Model):
+    first_name = models.CharField(max_length=30, default='')
+    last_name = models.CharField(max_length=30, default='')
+    relationship = models.CharField(max_length=12, default='')
+    member = models.ForeignKey(Member, on_delete=models.CASCADE, related_name='dependents', default='')
+
+    def __str__(self):
+        return self.first_name
